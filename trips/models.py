eb9c3f0e6909363f8f97992b7b5915930369d9c8
Adriana Palushi <adrianapalushi@gmail.com>
2022-03-26 15:27:03 +0100

    added class country

--- trips/models.py
+++ trips/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Continent(models.Model):
     name = models.CharField(max_length=30)
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    
+

4f9c657654f1721345545bbef4add330027eb999
Adriana Palushi <adrianapalushi@gmail.com>
2022-03-26 16:51:11 +0100

    added country and city models

--- trips/models.py
+++ trips/models.py
@@ -5,4 +5,19 @@ class Continent(models.Model):
 
 class Country(models.Model):
     name = models.CharField(max_length=50)
+    continent = models.ForeignKey(Continent, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = "country"
+        verbose_name_plural = "countries"
+    
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.ForeignKey(Country, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = "city"
+        verbose_name_plural = "cities"
     

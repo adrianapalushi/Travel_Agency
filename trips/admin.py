a54a390a17cbca17cac76c28737400e6b8246c18
Adriana Palushi <adrianapalushi@gmail.com>
2022-03-27 11:30:24 +0200

    added trip to admin

--- trips/admin.py
+++ trips/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Continent, Country, City, Hotel, Airport
+from .models import Continent, Country, City, Hotel, Airport, Trip
 
 admin.site.register(Continent)
 
@@ -9,9 +9,12 @@ admin.site.register(Country)
 admin.site.register(City)
 
 
+
 @admin.register(Hotel)
 class HotelAdmin(admin.ModelAdmin):
     list_display = ["name", "standard"]
 
 
+
 admin.site.register(Airport)
+admin.site.register(Trip)

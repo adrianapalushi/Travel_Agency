173757a8322a8d2e43607a95d1564cf4754d8bbb
Adriana Palushi <adrianapalushi@gmail.com>
2022-03-31 20:59:39 +0200

    added views and urls links

--- agency/settings.py
+++ agency/settings.py
@@ -47,7 +47,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR/ "Templates"
+            BASE_DIR / "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
